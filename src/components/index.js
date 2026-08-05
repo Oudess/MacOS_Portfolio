bc5e85a244a127a2dcea2c0368e1efa38239562f
Oudess <youssefbelabed28@gmail.com>
2026-08-05 21:53:36 +0100

    finished base code

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
 import Dock from "#components/Dock.jsx";
+import Home from "#components/Home.jsx";
 import WindowControls from "#components/WindowControls.jsx";
-export {Navbar, Welcome, Dock, WindowControls};
+export {Navbar, Welcome, Dock, WindowControls, Home};
